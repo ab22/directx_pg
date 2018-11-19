9c464a70e1c6ae4305f0945ecb34ba3277dfcb06
Abelardo E. Mendoza <abelardo22.9@gmail.com>
2018-11-18 22:36:35 -0600

    initial working gui with cmake

---
+++ main.cpp
@@ -0,0 +1,64 @@
+#include <Windows.h>
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR cmdLine, int cmdShow)
+{
+    static TCHAR appName[] = TEXT("DirectX Playground");
+    HWND     hwnd;
+    MSG      msg;
+    WNDCLASS wndclass;
+
+    wndclass.style = CS_HREDRAW | CS_VREDRAW;
+    wndclass.lpfnWndProc = WndProc;
+    wndclass.cbClsExtra = 0;
+    wndclass.cbWndExtra = 0;
+    wndclass.hInstance = hInstance;
+    wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+    wndclass.lpszMenuName = NULL;
+    wndclass.lpszClassName = appName;
+
+    if (!RegisterClass(&wndclass)) {
+        MessageBox(NULL, TEXT("Register Class Failed!"), appName, MB_ICONERROR);
+        return -1;
+    }
+
+    hwnd = CreateWindow(
+        appName,
+        TEXT("DirectX Playground"),
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT,
+        CW_USEDEFAULT, CW_USEDEFAULT,
+        NULL,
+        NULL,
+        hInstance,
+        NULL
+    );
+
+    ShowWindow(hwnd, cmdShow);
+    UpdateWindow(hwnd);
+
+    while(GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return (int)msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    switch(msg) {
+    case WM_CREATE:
+        return 0;
+
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+
+    }
+
+    return DefWindowProc(hwnd, msg, wParam, lParam);
+}
