93e09d3a18565ff44127057d45bdbdd90460dd4a
Abelardo E. Mendoza <abelardo22.9@gmail.com>
2018-12-04 00:08:24 -0600

    added com_ptr wrapper

---
+++ com_ptr.h
@@ -0,0 +1,59 @@
+#pragma once
+
+template<typename T>
+class ComPtr {
+private:
+	T* _obj = nullptr;
+
+public:
+	ComPtr()
+		: _obj(nullptr)
+	{
+	}
+
+	ComPtr(T* obj)
+		: _obj(obj)
+	{
+	}
+
+	~ComPtr() {
+		if (_obj != nullptr)
+			_obj->Release();
+	}
+
+	T* get() const
+	{
+		return _obj;
+	}
+
+	ULONG release()
+	{
+		if (_obj == nullptr)
+			return 0;
+
+		ULONG ref = _obj->Release();
+		_obj = nullptr;
+
+		return ref;
+	}
+
+	T** operator&()
+	{
+		return &_obj;
+	}
+
+	operator T*() const
+	{
+		return _obj;
+	}
+
+	T* operator->() const
+	{
+		return _obj;
+	}
+
+	bool operator!() const
+	{
+		return _obj != nullptr;
+	}
+};
